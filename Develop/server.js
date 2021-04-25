f754c386010f4609cb78f5f836fb4385cd4a1f00
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-04-25 19:44:16 +0100

    corrected files - just gitignore and server.js - minor change

--- Develop/server.js
+++ Develop/server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//need to change 'budget' to name of db - workoutdb
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
