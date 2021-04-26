3a2f1e7d18b1cec7430df7d734c0f0a476423ba4
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-04-26 23:54:31 +0100

    added monitor console.lot and app.use

--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,31 @@
-//from mini project
+
 
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-//need to change 'budget' to name of db - workoutdb
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+//need to change 'budget' to name of db - workout
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false, 
+  useUnifiedTopology: true,
+  useCreateIndex: true
+
 });
+
+mongoose.connection.on('connected', () => console.log('Connected to MongoDB Endpoint'));
+mongoose.connection.on('error', (err) => console.log(`Mongoose default connection error: ${err}`));
 
 // routes
 app.use(require("./routes/api.js"));
