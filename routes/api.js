c8e3a14592a8279e97a7d0ab9290381e68c78f80
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-04-28 17:01:01 +0100

    amended routes and server to use express Router correctly

--- routes/api.js
+++ routes/api.js
@@ -1,82 +1,84 @@
 
-const express = require("express");
+const router = require("express").Router();
 var db = require("../models");
 var Workout = db.Workout;
+
 
 var path = require("path");
 
 // connect to middleware
 
-module.exports = function (app) {
-    //get index.html page
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
 
-    //get exercise html page    
-    app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
+//get index.html page
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-    //get stats page
-    app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    });
+//get exercise html page    
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
-    //get all workouts
-    app.get("/all", (req, res) => {
-        db.Workout.find({})
-          .then(data => {
+//get stats page
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+//get all workouts
+router.get("/all", (req, res) => {
+    db.Workout.find({})
+        .then(data => {
             res.json(data);
-          })
-          .catch(err => {
+        })
+        .catch(err => {
             res.json(err);
-          });
-      });
+        });
+});
 
-    app.get("/api/workouts", (req, res) => {
-        Workout.find({})
-            .sort({ "day": "desc" })
-            .then(data => {
-                res.json(data);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .sort({ "day": "desc" })
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
- //   app.put("/api/workouts/:id", (req, res) => {
- //       Workout.findOneAndUpdate(
- //           { _id: req.params.id },
- //           { $push: { exercises: req.body } },
- //       )
- //           .then(data => {
- //               res.json(data)
- //           })
- //           .catch(err => {
- //               res.json(err);
- //           });
- //   });
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { exercises: req.body } },
+    )
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
- //   app.get("/api/workouts/range", function (req, res) {
- //       Workout.find({}).then(data => {
- //           res.json(data);
- //       })
- //           .catch(err => {
-  //              res.json(err);
-  //          });
-  // });
+router.get("/api/workouts/range", function (req, res) {
+    Workout.find({}).then(data => {
+        res.json(data);
+    })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
 
-    //POST create a new workout
-    //  app.post("/api/workouts", (req, res) => {
-    //      db.Workout.create({}).then(data => {
-    //        console.log("data", data);
-    //        res.json(data);
-    //      })
-    //        .catch(err => {
-    //          res.json(err);
-    //        });
-    //    })
-}
-module.exports = app;
+//  POST create a new workout
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create({}).then(data => {
+        console.log("data", data);
+        res.json(data);
+    })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-
-
+const db = require("./models")
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+
 
 const PORT = process.env.PORT || 3000;
 
